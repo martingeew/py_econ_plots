6330d7e1864b303d649aa8b0ec3938f12725755b
martingeew <67925955+martingeew@users.noreply.github.com>
2024-01-29 09:17:32 +0100

    add print statement of top and bottom movers for the quarter

--- cpi_inflation_distribution/src/features/002_transform_data.py
+++ cpi_inflation_distribution/src/features/002_transform_data.py
@@ -10,6 +10,18 @@ data = pd.read_pickle("../../data/interim/nz_cpi_group_3_clean_2023q4.pkl")
 # Calculate annual percent change
 cpi_data_apc=data.pct_change(periods=4) * 100
 
+# Display top and bottom movers
+
+index_to_filter = '2023Q4'
+
+filtered_df = cpi_data_apc.loc[[index_to_filter]]
+transposed_df=filtered_df.T
+transposed_df_sorted=transposed_df.sort_values(by=index_to_filter, ascending=True).round(2)
+print('Top 5 apc:')
+print(transposed_df_sorted.tail(3))
+print('\nBottom 5 apc:')
+print(transposed_df_sorted.head(3))
+
 # --------------------------------------------------------------
 # Export data
 # --------------------------------------------------------------
