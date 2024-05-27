a06bb1594a7e34936bffd183409ad200032c1f14
martingeew <67925955+martingeew@users.noreply.github.com>
2024-05-27 09:30:20 +0200

    Add population projection data

--- nz_demographics/src/data/nz_demographics_process_data.py
+++ nz_demographics/src/data/nz_demographics_process_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+############### Population estimates by TA2 #####################
+
 # Load the Excel file into a DataFrame
-df = pd.read_csv('population_data_nz_20240417.csv', header=[0, 1], sep=',')
+df = pd.read_csv('../../data/raw/population_data_nz_20240417.csv', header=[0, 1], sep=',')
 
 # Resetting the column names to a single level by joining the multi-index levels
 df.columns = [' '.join(col).strip() for col in df.columns.values]
@@ -22,9 +24,32 @@ df_long.drop(columns='Age and Year', inplace=True)
 df_long=df_long.rename(columns={'Age Year at 30 June':'Region'})
 
 # save df to csv
-df_long.to_csv('pop_estimate_interim.csv', index=False) 
+df_long.to_csv('../../data/interim/pop_estimate_interim.csv', index=False) 
+
+############### Population projections by TA2 #####################
+import pandas as pd
+
+# Load the Excel file into a DataFrame
+df = pd.read_csv('../../data/raw/pop_projections_TA2.csv', header=[0, 1], sep=',')
+
+# Resetting the column names to a single level by joining the multi-index levels
+df.columns = [' '.join(col).strip() for col in df.columns.values]
+
+# Melting the DataFrame to transform it from wide to long format
+df_long = pd.melt(df, id_vars=['Age Year at 30 June'], var_name='Age and Year', value_name='Population')
 
 
+# Extracting the year from the 'Age and Year' column
+df_long['Year'] = df_long['Age and Year'].str.extract(r'(\d{4})$')
 
+# Cleaning the 'Age' column by removing any sequence of digits that are formatted like a year
+df_long['Age'] = df_long['Age and Year'].str.slice(start=0, stop=-5)
+
+# Dropping the 'Age and Year' column as it's no longer needed
+df_long.drop(columns='Age and Year', inplace=True)
+
+df_long=df_long.rename(columns={'Age Year at 30 June':'Region'})
+
+df_long.to_csv('../../data/interim/pop_projection_interim.csv', index=False) 
 
 
